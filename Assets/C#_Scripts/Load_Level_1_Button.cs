f5908c4ceb7f37e78c57028f85822b0e12cf0970
Cdoran471 <cdoran@ltu.edu>
2022-10-26 17:02:45 -0400

    Main_Scene_1.1
    Focused on making the main scene

---
+++ Assets/C#_Scripts/Load_Level_1_Button.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_Level_1_Button : MonoBehaviour
+{
+    public void LoadLevel(string Level_1)
+    {
+        SceneManager.LoadScene(Level_1);
+    }
+}
